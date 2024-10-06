45c170de4cbd06043c2fd8924e068a186d65513e
Sandrine IBRAIMA-NOMBO <dsirci@yahoo.fr>
2024-10-06 23:36:20 +0200

    créations des pages clients et commandes

---
+++ front/src/app/clients/page.tsx
@@ -0,0 +1,9 @@
+const Clients = () => {
+    return (
+        <div>
+            <h2>Nos clients</h2>
+        </div>
+    )
+}
+
+export default Clients;
---
+++ front/src/app/orders/page.tsx
@@ -0,0 +1,9 @@
+const Orders = () => {
+    return (
+        <div>
+            <h2>Nos commandes</h2>
+        </div>
+    )
+}
+
+export default Orders;
